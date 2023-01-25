8a302ac9fd66e6fd3e79dc2722722fad8cb27a70
onaries <seonwookim@gm.gist.ac.kr>
2023-01-25 22:51:15 +0900

    Timezone 설정 추가

--- src/main/java/com/example/accountbook/AccountBookApplication.java
+++ src/main/java/com/example/accountbook/AccountBookApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class AccountBookApplication {
@@ -12,4 +16,9 @@ public class AccountBookApplication {
         SpringApplication.run(AccountBookApplication.class, args);
     }
 
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("실행시각: " + new Date());
+    }
 }
